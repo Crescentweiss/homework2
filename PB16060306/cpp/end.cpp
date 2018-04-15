98af49ff12c7da936b263ccf5afc43b3be6a926e
Crescentweiss <36957992+Crescentweiss@users.noreply.github.com>
2018-04-16 03:06:44 +0800

    Update end.cpp

--- PB16060306/cpp/end.cpp
+++ PB16060306/cpp/end.cpp
@@ -1,28 +1,58 @@
-#ifndef SETTING_H
-#define SETTING_H
+#include "end.h"
+#include "ui_end.h"
+#include <form.h>
+#include <ui_form.h>
+#include "error.h"
+#include "ui_error.h"
+#include "score.h"
+#include "ui_score.h"
+#include "history.h"
+#include "ui_history.h"
 
-#include <QWidget>
+end::end(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::end)
+{
+    ui->setupUi(this);
 
-namespace Ui {
-class Setting;
+    //connect(ui->back,SIGNAL(clicked()),this,SLOT(on_back_clicked()));
+    //connect(ui->error,SIGNAL(clicked()),this,SLOT(on_error_clicked()));
+    //connect(ui->score,SIGNAL(clicked()),this,SLOT(on_score_clicked()));
+    //connect(ui->history,SIGNAL(clicked()),this,SLOT(on_history_clicked()));
 }
 
-class Setting : public QWidget
+end::~end()
 {
-    Q_OBJECT
+    delete ui;
+}
 
-public:
-    explicit Setting(QWidget *parent = 0);
-    ~Setting();
+void end::on_back_clicked()
+{
+    Form *form=new Form;
+    this->hide();
+    form->show();
+}
 
-public slots:
-    void confirm(); //确认参数设置
+void end::on_error_clicked()
+{
+    error *ERROR=new error;
+    ERROR->show();
+}
 
-private slots:
-    void on_pushButton_clicked();
+void end::on_score_clicked()
+{
+    score *SCORE=new score;
+    SCORE->show();
+}
 
-private:
-    Ui::Setting *ui;
-};
+void end::on_history_clicked()
+{
+    history *HISTORY=new history;
+    HISTORY->show();
+}
 
-#endif // SETTING_H
+
+void end::on_close_clicked()
+{
+    this->close();
+}
